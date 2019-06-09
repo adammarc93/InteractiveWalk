f950600b2bb4d20ae894465aae22474ed7b7d527
adammarc93 <adammarc93@gmail.com>
2019-06-10 01:45:01 +0200

    Added sound effect to level portal

--- Assets/Scripts/LoadNewLevel.cs
+++ Assets/Scripts/LoadNewLevel.cs
@@ -7,17 +7,24 @@ public class LoadNewLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider colider)
     {
+        GetComponent<AudioSource>().Play();
+        StartCoroutine("GoToNewLevel");
+    }
+
+    IEnumerator GoToNewLevel()
+    {
+        yield return new WaitForSeconds(2f);
         Application.LoadLevel("Level 2");
     }
 }
