605ee118596c8cda22973ef7fe9c06b799f6a789
adammarc93 <adammarc93@gmail.com>
2019-06-09 23:58:30 +0200

    Added functionality to panel menu

---
+++ Assets/Scripts/LevelsLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevelOne()
+    {
+        Application.LoadLevel("Level 1");
+    }
+
+    public void LoadLevelTwo()
+    {
+        Application.LoadLevel("Level 2");
+    }
+}
