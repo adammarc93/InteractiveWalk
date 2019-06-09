3faf36697337229781326e6599734126b11c6686
adammarc93 <adammarc93@gmail.com>
2019-06-10 00:23:04 +0200

    Added audio and audio controller

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public GameObject audioContainer;
+    private bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchMusic()
+    {
+        if (!isActive)
+        {
+            audioContainer.SetActive(true);
+            isActive = true;
+        }
+        else
+        {
+            audioContainer.SetActive(false);
+            isActive = false;
+        }
+    }
+}
