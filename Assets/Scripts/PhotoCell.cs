fb5f0f73eaf433b348eb24d17a0d3b8a1e88ba87
adammarc93 <adammarc93@gmail.com>
2019-06-09 21:26:25 +0200

    Added assets to scene and photocell

---
+++ Assets/Scripts/PhotoCell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoCell : MonoBehaviour
+{
+    public GameObject spotLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        spotLight.SetActive(true);
+    }
+}
